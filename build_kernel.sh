0a62f481f9e1973216fa640c79369eb26a61b5a5
Rappelkiste98 <niklas.1998@live.de>
2024-03-11 23:31:09 +0000

    Add Linux Module + Fix Build-Script

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,11 +1,18 @@
 #! /bin/bash -e
-run_dir=/home/quenter/build-pve
+work_dir=$(pwd)
 
 # Prepare Raspberry-Linux
-sleep 5
+echo "\n============================ ! Prepare System APT ! ============================\n"
+
+sudo apt install git bc bison flex libssl-dev make libc6-dev libncurses5-dev crossbuild-essential-arm64 -y
+sudo apt install curl:arm64 zlib1g-dev:arm64 libblkid-dev:arm64 uuid-dev:arm64 libtirpc-dev:arm64 libudev-dev:arm64 libcrypt-dev:arm64 libssl-dev:arm64 libaio-dev:arm64 libattr1-dev:arm64 libelf-dev:arm64 libffi-dev:arm64 libcurl4-openssl-dev:arm64 libtool-bin -y
+sudo apt install build-essential autoconf automake libtool gawk alien fakeroot dkms libblkid-dev uuid-dev libudev-dev libssl-dev zlib1g-dev libaio-dev libattr1-dev libelf-dev linux-headers-generic python3 python3-dev python3-setuptools python3-cffi libffi-dev python3-packaging git libcurl4-openssl-dev debhelper-compat dh-python po-debconf python3-all-dev python3-sphinx parallel -y
+
+# Prepare Raspberry-Linux
+sleep 10
 echo "\n============================ ! Prepare Raspberry-Linux ! ============================\n"
 
-cd $repo_dir/raspberry-pve-linux
+cd $work_dir/linux
 
 git reset --hard
 
@@ -34,7 +41,7 @@ make prepare scripts -j$(nproc)
 sleep 5
 echo "\n============================ ! Build ZFS ! ============================\n"
 
-cd ./zfs
+cd $work_dir/zfs
 
 git reset --hard
 
@@ -46,8 +53,8 @@ CC=aarch64-linux-gnu-gcc \
 ./configure \
 --build=x86_64-linux-gnu --host=aarch64-linux-gnu --target=aarch64-linux-gnu \
 --enable-linux-builtin \
---with-linux=$repo_dir/raspberry-pve-linux \
---with-linux-obj=$repo_dir/raspberry-pve-linux
+--with-linux=$work_dir/linux \
+--with-linux-obj=$work_dir/linux
 
 make -s -j$(nproc)
 
