660cc8b62cdbe29edbd1fad0e1cb53b55e4695da
Rappelkiste98 <niklas.1998@live.de>
2024-12-07 01:59:19 +0100

    upgrade to 6.6.51

--- build_kernel.sh
+++ build_kernel.sh
@@ -11,7 +11,7 @@ sudo apt update
 
 sudo apt install git bc bison flex libssl-dev make libc6-dev libncurses5-dev crossbuild-essential-arm64 -y
 sudo apt install zlib1g-dev:arm64 libblkid-dev:arm64 uuid-dev:arm64 libtirpc-dev:arm64 libudev-dev:arm64 libcrypt-dev:arm64 libssl-dev:arm64 libaio-dev:arm64 libattr1-dev:arm64 libelf-dev:arm64 libffi-dev:arm64 libcurl4-openssl-dev:arm64 libtool-bin -y
-sudo apt install build-essential autoconf automake libtool gawk alien fakeroot dkms libblkid-dev uuid-dev libudev-dev libssl-dev zlib1g-dev libaio-dev libattr1-dev libelf-dev linux-headers-generic python3 python3-dev python3-setuptools python3-cffi libffi-dev python3-packaging git libcurl4-openssl-dev debhelper-compat dh-python po-debconf python3-all-dev python3-sphinx parallel rsync -y
+sudo apt install build-essential autoconf automake libtool gawk alien fakeroot dkms libblkid-dev uuid-dev libudev-dev libssl-dev zlib1g-dev libaio-dev libattr1-dev libelf-dev linux-headers-generic python3 python3-dev python3-setuptools python3-cffi libffi-dev python3-packaging git debhelper-compat dh-python po-debconf python3-all-dev python3-sphinx parallel rsync -y
 
 # Prepare Raspberry-Linux
 sleep 10
